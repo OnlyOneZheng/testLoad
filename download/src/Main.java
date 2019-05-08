170c50a4cd01d17699f1e4fc062a058268dca409
ZZR_200 <766216302@qq.com>
2019-05-08 16:15:42 +0800

    第一次提交下载

---
+++ download/src/Main.java
@@ -0,0 +1,29 @@
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String str="http://media.febcchinese.org/streaming/vfs/mavdp001.mp3";
+        String substring2 = str.substring(0, str.length() - 4);
+        writeWeb(substring2);
+        //System.out.println("Hello World!");
+    }
+
+    public static void  writeWeb(String str1){
+        int numb = Integer.parseInt((str1.substring(str1.length() - 3, str1.length())));
+        System.out.println(numb);
+
+
+
+        for (int i = 0;i<31;i++ ) {
+
+            DecimalFormat df=new DecimalFormat("000");
+            String str2=df.format(numb+i);
+            String str=str1.substring(0 , str1.length()-3)+(str2)+".mp3";
+            System.out.println(str);
+        }
+    }
+}
